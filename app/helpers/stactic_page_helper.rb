891f11edccf684dd52ad439e4a961ad0512b2356
NgocSon <sonnn.qni@gmail.com>
2021-01-26 09:55:25 +0700

    chapter 3,4,5

---
+++ app/helpers/stactic_page_helper.rb
@@ -0,0 +1,2 @@
+module StacticPageHelper
+end
---
+++ test/controllers/stactic_page_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StacticPageControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
