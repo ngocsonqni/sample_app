2cec7e1d05307e435ecab0acb8f47932e0c8faae
NgocSon <sonnn.qni@gmail.com>
2021-01-28 08:19:02 +0700

    chapter_8

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,20 @@ class User < ApplicationRecord
             length: {maximum: 500},
             format: {with: VALID_EMAIL_REGEX}
   validates :password, length: {minimum: 6}
-
   before_save :downcase_email
-
   has_secure_password
-  private
 
+  # Returns the hash digest of the given string.
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create string, cost: cost
+  end
+
+  private
   def downcase_email
     email.downcase!
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+
   get "static_page/home"
   get "static_page/help"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
-  # resources :users, only: %i(new create)
+  resources :sessions, only: %i(new create)
   scope "(:locale)", locale: /en|vi/ do
     resources :users, except: %i(new create)
   end
 end
+
