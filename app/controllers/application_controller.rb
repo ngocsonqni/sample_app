cd34d742ad3230c4f6e2c652c0cd88e5f85ca25e
NgocSon <sonnn.qni@gmail.com>
2021-02-01 14:26:26 +0700

    chapter 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@
 
 # ApplicationController
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
   before_action :set_locale
 
   def set_locale
