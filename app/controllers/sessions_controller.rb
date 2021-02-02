f176ffc505190c84172f2f2a3ca1168c18b30a0c
NgocSon <sonnn.qni@gmail.com>
2021-02-02 16:04:57 +0700

    chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 class SessionsController < ApplicationController
+  before_action :load_user, only: :create
+
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+    if @user.try(:authenticate, params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_to @user
     else
-      flash.now[:danger] = t("invalid_email_password")
+      flash.now[:danger] = t "invalid_email_password"
       render :new
     end
   end
@@ -15,5 +17,15 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to static_pages_home_path
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by email: params[:session][:email].downcase
+    return if @user
+
+    flash[:danger] = t "email_not_found"
+    redirect_to login_path
   end
 end
